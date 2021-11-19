98acfd1f4f9ba1d173060da172dc93909e8b154f
David Svensson <davidsvson@gmail.com>
2021-11-19 12:03:40 +0100

    first commit

--- GestureExperiments/ViewController.swift
+++ GestureExperiments/ViewController.swift
@@ -14,6 +14,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func catTaped(_ sender: UITapGestureRecognizer) {
+        print("cat tapped!!")
+    }
+    
+    @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+        
+        let translation = sender.translation(in: self.view)
+        
+        if let viewToPan = sender.view {
+            viewToPan.center = CGPoint(x: viewToPan.center.x + translation.x,
+                                       y: viewToPan.center.y + translation.y)
+        }
+        
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
+    }
 }
 
